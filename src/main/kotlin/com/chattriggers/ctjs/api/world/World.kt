76d2570e9462a3f653bffc52081ed17c70413aa0
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-04 11:27:46 -0700

    fix compile errors

--- src/main/kotlin/com/chattriggers/ctjs/api/world/World.kt
+++ src/main/kotlin/com/chattriggers/ctjs/api/world/World.kt
@@ -75,7 +75,8 @@ object World {
     fun getDifficulty(): Settings.Difficulty? = toMC()?.difficulty?.let(Settings.Difficulty::fromMC)
 
     @JvmStatic
-    fun getMoonPhase(): Int = toMC()?.moonPhase?.index ?: -1
+    // FIXME
+    fun getMoonPhase(): Int = -1 // toMC()?.moonPhase?.index ?: -1
 
     /**
      * Gets the [Block] at a location in the world.
