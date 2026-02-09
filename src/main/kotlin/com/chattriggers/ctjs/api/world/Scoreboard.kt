b1ce8e2a0bcd5c73da7b2dba33415a30123c6674
Doc <65479309+DocilElm@users.noreply.github.com>
2026-02-09 12:43:12 -0700

    pushing my 3mo ago changes (DOES NOT WORK)

--- src/main/kotlin/com/chattriggers/ctjs/api/world/Scoreboard.kt
+++ src/main/kotlin/com/chattriggers/ctjs/api/world/Scoreboard.kt
@@ -7,6 +7,9 @@ import com.chattriggers.ctjs.api.message.TextComponent
 import com.chattriggers.ctjs.internal.mixins.`Scoreboard$1Accessor`
 import com.chattriggers.ctjs.internal.utils.asMixin
 import gg.essential.elementa.state.BasicState
+import net.minecraft.network.chat.Style
+import net.minecraft.network.chat.numbers.NumberFormat
+import net.minecraft.network.chat.numbers.StyledFormat
 import net.minecraft.scoreboard.ScoreAccess
 import net.minecraft.scoreboard.ScoreboardDisplaySlot
 import net.minecraft.scoreboard.ScoreboardObjective
@@ -14,6 +17,9 @@ import net.minecraft.scoreboard.ScoreboardScore
 import net.minecraft.scoreboard.number.NumberFormat
 import net.minecraft.scoreboard.number.StyledNumberFormat
 import net.minecraft.text.Style
+import net.minecraft.world.scores.DisplaySlot
+import net.minecraft.world.scores.Objective
+import net.minecraft.world.scores.ScoreAccess
 import org.mozilla.javascript.NativeObject
 
 object Scoreboard {
@@ -31,7 +37,7 @@ object Scoreboard {
     fun getScoreboard() = toMC()
 
     @JvmStatic
-    fun getSidebar(): ScoreboardObjective? = toMC()?.getObjectiveForSlot(ScoreboardDisplaySlot.SIDEBAR)
+    fun getSidebar(): Objective? = toMC()?.getDisplayObjective(DisplaySlot.SIDEBAR)
 
     /**
      * Gets the top-most string which is displayed on the scoreboard. (doesn't have a score on the side).
@@ -125,10 +131,10 @@ object Scoreboard {
             return
         }
 
-        scoreboard.knownScoreHolders.forEach {
-            val scoreboardScore = scoreboard.getScore({ it.nameForScoreboard }, sidebarObjective) as? ScoreboardScore
-            if (scoreboardScore?.score == score) {
-                scoreboardScore.displayText = line
+        scoreboard.trackedPlayers.forEach {
+            val scoreboardScore = scoreboard.getPlayerScoreInfo({ it.scoreboardName }, sidebarObjective) as? net.minecraft.world.scores.Score
+            if (scoreboardScore?.value() == score) {
+                scoreboardScore.display(line)
             }
         }
     }
@@ -148,9 +154,9 @@ object Scoreboard {
         val scoreboard = toMC() ?: return
         val sidebarObjective = getSidebar() ?: return
 
-        val newLine = scoreboard.getOrCreateScore({ Math.random().toString() }, sidebarObjective, true)
-        newLine.displayText = line
-        newLine.score = score
+        val newLine = scoreboard.getOrCreatePlayerScore({ Math.random().toString() }, sidebarObjective, true)
+        newLine.display(line)
+        newLine.set(score)
 
         updateNames()
     }
@@ -195,7 +201,7 @@ object Scoreboard {
      * @param name the name of the team
      */
     @JvmStatic
-    fun createTeam(name: String): Team = Team(toMC()!!.addTeam(name))
+    fun createTeam(name: String): Team = Team(toMC()!!.addPlayerTeam(name))
 
     private fun updateNames() {
         scoreboardNames.clear()
@@ -209,10 +215,10 @@ object Scoreboard {
         if (!customTitle)
             scoreboardTitle = TextComponent(objective.displayName)
 
-        val newScores = scoreboard.knownScoreHolders.asSequence().filter {
-            objective in scoreboard.getScoreHolderObjectives(it)
+        val newScores = scoreboard.trackedPlayers.asSequence().filter {
+            objective in scoreboard.listPlayerScores(it)
         }.map {
-            scoreboard.getOrCreateScore(it, objective, true)
+            scoreboard.getOrCreatePlayerScore(it, objective, true)
         }.mapTo(mutableListOf(), ::Score)
 
         scoreboardNames = newScores.sortedWith(compareBy<Score> {
@@ -233,8 +239,8 @@ object Scoreboard {
     }
 
     class Score(override val mcValue: ScoreAccess) : CTWrapper<ScoreAccess> {
-        private val scoreState = BasicState(mcValue.score)
-        private val nameState = BasicState(mcValue.displayText)
+        private val scoreState = BasicState(mcValue.get())
+        private val nameState = BasicState(mcValue.display())
         private val formatState = BasicState(mcValue.asMixin<`Scoreboard$1Accessor`>().score.numberFormat)
         private val teamState = run {
             val scoreboard = Scoreboard.toMC()!!
@@ -285,7 +291,7 @@ object Scoreboard {
          */
         fun setScore(score: Int) = apply {
             scoreState.set(score)
-            mcValue.score = score
+            mcValue.set(score)
         }
 
         /**
@@ -297,7 +303,7 @@ object Scoreboard {
             val name = mcValue.asMixin<`Scoreboard$1Accessor`>().holder.nameForScoreboard
 
             return TextComponent(
-                MCTeam.decorateName(
+                MCTeam.formatNameForTeam(
                     getTeam()?.mcValue,
                     TextComponent(nameState.get() ?: name),
                 )
@@ -312,7 +318,7 @@ object Scoreboard {
          */
         fun setName(name: TextComponent?) = apply {
             nameState.set(name)
-            mcValue.displayText = name
+            mcValue.display(name)
         }
 
         /**
@@ -333,15 +339,15 @@ object Scoreboard {
          */
         fun setNumberFormat(format: Any?) = apply {
             val style = when (format) {
-                is CharSequence -> StyledNumberFormat(TextComponent(format.toString()).style)
-                is NativeObject -> StyledNumberFormat(TextComponent.jsObjectToStyle(format))
+                is CharSequence -> StyledFormat(TextComponent(format.toString()).style)
+                is NativeObject -> StyledFormat(TextComponent.jsObjectToStyle(format))
                 is NumberFormat -> format
-                is Number -> StyledNumberFormat(Style.EMPTY.withColor(format.toInt()))
+                is Number -> StyledFormat(Style.EMPTY.withColor(format.toInt()))
                 else -> null
             }
 
             formatState.set(style)
-            mcValue.setNumberFormat(style)
+            mcValue.numberFormatOverride(style)
         }
 
         /**
